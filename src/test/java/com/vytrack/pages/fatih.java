d02c4421afc0df5064908c06894b16394c45c395
fatih <fatihatasoypa@gmail.com>
2022-08-19 22:54:18 +0300

    atasoy locater is added

--- src/test/java/com/vytrack/pages/fatih.java
+++ src/test/java/com/vytrack/pages/fatih.java
@@ -2,14 +2,20 @@ package com.vytrack.pages;
 
 import com.vytrack.utilities.Driver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class fatih  {
-public fatih() {    PageFactory.initElements(Driver.get(),this);}
+public class fatih {
+    public fatih() {
+        PageFactory.initElements(Driver.get(), this);
+    }
 
 
-    @FindBy (id = "dd")
+    @FindBy(id = "dd")
     public WebDriver aa;
+
+    @FindBy(id = "atasoy")
+    public WebElement thisIsAtasoyLocater;
 
 }
